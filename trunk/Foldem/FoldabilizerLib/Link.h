fc36f3951d6eb671c167028f458a7f2c629e17dc
howard.hhli@gmail.com <howard.hhli@gmail.com@ecd451a5-5fcc-73d8-86e1-6e419f31a703>
2013-11-04 05:47:08 +0000

    minor in Link

--- trunk/Foldem/FoldabilizerLib/Link.h
+++ trunk/Foldem/FoldabilizerLib/Link.h
@@ -2,26 +2,17 @@
 
 #include "FoldabilizerLibGlobal.h"
 
-//#include "Node.h"
 class Node;
 
 class Link
 {
 public:
     Link();
-    Link(Point &f, Point &t, Node* n1, Node* n2);
+    Link(Node* n1, Node* n2);
     ~Link(){}
 
 public:
-	//Hinge score
-	double score;
-	//Is folded(true) or not(false)
-	bool isFolded;
-    //Angle between two cuboids
 	double angle;
-    //Position to attach the hinge
-	Point from;
-	Point to;
 
 public:
     void setNode(Node* n1, Node* n2);
@@ -29,14 +20,7 @@ public:
     Node* getNode1();
     Node* getNode2();
 
-	//Visualize Hinge as a line
 	void draw();
-
-	//Calculate angle 
-	double calAngle();
-
-	//Calculate folding score
-	double calScore();
 						  
 private:
     Node *node1;
