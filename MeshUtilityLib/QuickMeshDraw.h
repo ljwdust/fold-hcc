681e791c087521f46526330ee0ada321c22de0cf
ennetws <ennetws@ecd451a5-5fcc-73d8-86e1-6e419f31a703>
2014-05-30 05:32:51 +0000

    Cosmetic changes.

--- MeshUtilityLib/QuickMeshDraw.h
+++ MeshUtilityLib/QuickMeshDraw.h
@@ -70,6 +70,48 @@ struct QuickMeshDraw{
 		glDisable(GL_CULL_FACE);
 	}
 
+	static void drawMeshSharpEdges( SurfaceMeshModel * mesh, QColor color, float lineWidth )
+	{
+		if(!mesh) return;
+
+		Surface_mesh::Face_iterator fit, fend = mesh->faces_end();
+		Surface_mesh::Vertex_around_face_circulator fvit, fvend;
+		Surface_mesh::Vertex_property<Vector3> points = mesh->vertex_property<Vector3>("v:point");
+		Surface_mesh::Face_property<Vector3> fnormals = mesh->face_property<Vector3>("f:normal");
+
+		glDisable(GL_LIGHTING);
+
+		glEnable (GL_BLEND);
+		glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+		glColor4d(color.redF(),color.greenF(),color.blueF(),1);
+		glLineWidth( lineWidth );
+		glEnable(GL_CULL_FACE);
+		glCullFace(GL_BACK);
+
+		double threshold = cos( M_PI_4 );
+
+		glBegin(GL_LINES);
+		foreach(Edge e, mesh->edges())
+		{
+			Vector3 n0 = fnormals[mesh->face(mesh->halfedge(e,0))];
+			Vector3 n1 = fnormals[mesh->face(mesh->halfedge(e,1))];
+
+			if( dot( n0, n1 ) > threshold ) continue;
+
+			Vector3 p0 = points[mesh->vertex(e, 0)];
+			Vector3 p1 = points[mesh->vertex(e, 1)];
+
+			glVertex3dv(p0.data());
+			glVertex3dv(p1.data());
+		}
+		glEnd();
+
+		glDisable(GL_CULL_FACE);
+
+		glEnable(GL_LIGHTING);
+	}
+
 	static void drawMeshName( SurfaceMeshModel * mesh, int name = 0 )
 	{
 		glPushName( name );
